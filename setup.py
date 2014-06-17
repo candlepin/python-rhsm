d5010166a63db8ed581193552188675bd48de4f8
barnabycourt <bcourt@redhat.com>
2014-06-17 12:27:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     include_package_data = True,
 
     ext_modules=[Extension('rhsm._certificate', ['src/certificate.c'],
-                           libraries=['ssl'])],
+                           libraries=['ssl'], extra_compile_args=['-O2'])],
 
     test_suite = 'nose.collector',
 
